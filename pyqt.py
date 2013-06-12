70d418d80b8ae776f7622a324e0233cbafde86f9
S4AD <dz8sot@gmail.com>
2013-06-12 15:08:54 +0200

    Update pyqt.py

--- pyqt.py
+++ pyqt.py
@@ -7,7 +7,7 @@ from PyQt4 import QtCore
 
 
 class Icon(QtGui.QWidget):
-    def __init__(self , parent=None):
+	def __init__(self , parent=None):
 		QtGui.QWidget.__init__(self, parent)
 		
 		color = QtGui.QColor(0,0,0)
